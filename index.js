bf5544434f902b284f0a0c9fb6cd797c25dcb28d
mohamedalaa <mohamed_ll14@yahoo.com>
2018-12-20 23:48:28 +0200

    update

--- index.js
+++ index.js
@@ -14,12 +14,16 @@ app.use(bodyParser.json());// parse application/json
 app.use('/api', require('./routers/api'));
 // Add headers
 app.use( (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3030');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3030');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // res.setHeader('Access-Control-Allow-Credentials', true);    
     next();
 });
 // app.use(cors());
