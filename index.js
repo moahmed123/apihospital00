f1f49182acae153c37ff3f8e1bc69b7431424c56
mohamedalaa <mohamed_ll14@yahoo.com>
2018-12-23 19:12:36 +0200

    dash

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ app.use(bodyParser.json());// parse application/json
 app.use('/api', require('./routers/api'));
 // Add headers
 app.use( (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://dashboardmobile.herokuapp.com');
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://dashboardmobile.herokuapp.com/');
+    // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3030');
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // res.setHeader('Access-Control-Allow-Credentials', true);    
+    // Pass to next layer of middleware
     next();
 });
 // app.use(cors());
